0dacc6d0b9179f87bb72e6c9b9949b5a5a89d41e
Jan Hähne <github@jhae.de>
2023-11-01 17:51:22 +0100

    Fix custom messages
    Added rule name to custom messages of
    - `scss/at-function-pattern` rule
    - `scss/at-mixin-pattern` rule
    - `scss/dollar-variable-pattern` rule
    - `scss/percent-placeholder-pattern` rule

---
+++ tests/scss/at-function-pattern.test.ts
@@ -0,0 +1,37 @@
+import { RuleTest } from '@jhae/stylelint-rule-tester';
+
+RuleTest.setConfigFile('index.yaml');
+
+RuleTest.describe(
+  'scss/at-function-pattern',
+  {
+    name: 'Require kebab-case for function names',
+    code: `
+      @function FOO-bar ();
+      @function foo-BAR ();
+      @function FOO-BAR ();
+      @function FOO_bar ();
+      @function foo_BAR ();
+      @function FOO_BAR ();
+      @function foo_bar ();
+      @function Foobar ();
+      @function FooBar ();
+      @function fooBar ();
+      @function _foo-bar ();
+    `,
+    expect: {
+      errored: true,
+      messages: new Array(11).fill('Expected function name to be kebab-case'),
+      severities: new Array(11).fill('error'),
+    },
+  },
+  {
+    name: 'Require kebab-case for function names',
+    code: `
+      @function foo ();
+      @function foo-bar ();
+      @function foo1-bar-2 ();
+      @function -foo-bar-baz ();
+    `,
+  },
+);
---
+++ tests/scss/at-mixin-pattern.test.ts
@@ -0,0 +1,37 @@
+import { RuleTest } from '@jhae/stylelint-rule-tester';
+
+RuleTest.setConfigFile('index.yaml');
+
+RuleTest.describe(
+  'scss/at-mixin-pattern',
+  {
+    name: 'Require kebab-case for mixin names',
+    code: `
+      @mixin FOO-bar ();
+      @mixin foo-BAR ();
+      @mixin FOO-BAR ();
+      @mixin FOO_bar ();
+      @mixin foo_BAR ();
+      @mixin FOO_BAR ();
+      @mixin foo_bar ();
+      @mixin Foobar ();
+      @mixin FooBar ();
+      @mixin fooBar ();
+      @mixin _foo-bar ();
+    `,
+    expect: {
+      errored: true,
+      messages: new Array(11).fill('Expected mixin name to be kebab-case'),
+      severities: new Array(11).fill('error'),
+    },
+  },
+  {
+    name: 'Require kebab-case for mixin names',
+    code: `
+      @mixin foo ();
+      @mixin foo-bar ();
+      @mixin foo1-bar-2 ();
+      @mixin -foo-bar-baz ();
+    `,
+  },
+);
