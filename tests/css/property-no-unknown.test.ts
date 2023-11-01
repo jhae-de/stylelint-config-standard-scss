110fab2676edb06d47d70bf0da6d2336c83258ca
Jan Hähne <github@jhae.de>
2023-11-01 17:48:18 +0100

    Add scss/property-no-unknown rule

--- tests/css/property-no-unknown.test.ts
+++ tests/css/property-no-unknown.test.ts
@@ -2,27 +2,16 @@ import { RuleTest } from '@jhae/stylelint-rule-tester';
 
 RuleTest.setConfigFile('index.yaml');
 
-RuleTest.describe(
-  'property-no-unknown',
-  {
-    name: 'Disallow unknown properties',
-    code: `
-      test { colr: black; }
-      test { my-property: 1; }
-    `,
-    expect: {
-      errored: true,
-      messages: ['Unexpected unknown property "colr"', 'Unexpected unknown property "my-property"'],
-      severities: ['error', 'error'],
-    },
-  },
-  {
-    name: 'Allow unknown prefixed properties',
-    code: `
-      test {
-        -moz-align-self: center;
-        -moz-overflow-scrolling: center;
-      }
-    `,
-  },
-);
+// This rule is disabled in favor of scss/property-no-unknown, so unknown properties should be allowed.
+
+RuleTest.describe('property-no-unknown', {
+  name: 'Allow unknown properties',
+  code: `
+    test {
+      colr: black;
+      my-property: 1;
+      -moz-align-self: center;
+      -moz-overflow-scrolling: center;
+    }
+  `,
+});
