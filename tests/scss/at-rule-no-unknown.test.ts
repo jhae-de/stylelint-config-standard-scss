2bf1d5bde01c098c4770a87984fb03012ba3126d
Jan Hähne <github@jhae.de>
2024-09-24 01:20:19 +0200

    Allow unknown @tailwind at-rule

--- tests/scss/at-rule-no-unknown.test.ts
+++ tests/scss/at-rule-no-unknown.test.ts
@@ -2,12 +2,23 @@ import { RuleTest } from '@jhae/stylelint-rule-tester';
 
 RuleTest.setConfigFile('index.yaml');
 
-RuleTest.describe('scss/at-rule-no-unknown', {
-  name: 'Disallow unknown at-rules',
-  code: '@unknown {}',
-  expect: {
-    errored: true,
-    messages: ['Unexpected unknown at-rule "@unknown"'],
-    severities: ['error'],
+RuleTest.describe(
+  'scss/at-rule-no-unknown',
+  {
+    name: 'Disallow unknown at-rules',
+    code: '@unknown {}',
+    expect: {
+      errored: true,
+      messages: ['Unexpected unknown at-rule "@unknown"'],
+      severities: ['error'],
+    },
   },
-});
+  {
+    name: 'Allow unknown @tailwind at-rule',
+    code: `
+      @tailwind base;
+      @tailwind components;
+      @tailwind utilities;
+    `,
+  },
+);
